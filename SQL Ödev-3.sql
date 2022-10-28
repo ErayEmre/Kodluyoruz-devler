859d5d684f67f8d76e64e66c9cdbdca687285135
ErayEmre <116040419+ErayEmre@users.noreply.github.com>
2022-10-29 00:46:21 +0300

    Patika dev./Kodluyoruz SQL Ödev-3

--- SQL Ödev-3.sql
+++ SQL Ödev-3.sql
@@ -2,7 +2,7 @@ SELECT country FROM country WHERE country LIKE 'A%a';--1.sorunun cevabý
 
 SELECT country FROM country WHERE country LIKE '-----n';--2.sorunun cevabý
 
-SELECT title FROM film WHERE title ILIKE '%T%T%T%T';--3.sorunun cevabý
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';--3.sorunun cevabý
 
 SELECT title FROM film WHERE (title LIKE 'C%') AND (length > 90 AND rental_rate = 2.99);--4.sorunun cevabý
 
