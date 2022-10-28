74e359781f3850f48846b8512b59d297fff215b5
ErayEmre <116040419+ErayEmre@users.noreply.github.com>
2022-10-29 01:42:05 +0300

    Patika dev./Kodluyoruz SQL Ödev-5

---
+++ SQL Ödev-5.sql
@@ -0,0 +1,6 @@
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC; --1.sorunun cevabý
+
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length OFFSET 5 LIMIT 5; --2.sorunun cevabý
+
+SELECT * FROM customer WHERE store_id=1 ORDER BY last_name DESC LIMIT 4;--3.sorunun cevabý
+
